1f9d11447b17a1f7b28dd700d27fd861b8b1d74c
gherardovarando <gherardo.varando@gmail.com>
2018-05-14 15:00:46 +0200

    v1.4.0

--- app/main.js
+++ app/main.js
@@ -31,6 +31,8 @@ const {
 } = require('electron')
 
 
+require('update-electron-app')()
+
 // Keep a global reference of the window object, if you don't, the window will
 // be closed automatically when the JavaScript object is garbage collected.
 let win
